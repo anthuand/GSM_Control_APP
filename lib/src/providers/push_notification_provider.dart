3e4d6b1149ee7fbc78a0ee90843f5631c5f50d6a
anthuand <anthuandamas@gmail.com>
2021-08-21 13:01:35 -0400

    Build apk

--- lib/src/providers/push_notification_provider.dart
+++ lib/src/providers/push_notification_provider.dart
@@ -1,42 +1,42 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:firebase_messaging/firebase_messaging.dart';
 
-class PushNotification {
-  static FirebaseMessaging messaging = FirebaseMessaging.instance;
-  static String? token;
+// class PushNotification {
+//   static FirebaseMessaging messaging = FirebaseMessaging.instance;
+//   static String? token;
 
-  static Future _backgroundHandler(RemoteMessage message) async{
-    print('Background Handler  ${message.messageId}');
+//   static Future _backgroundHandler(RemoteMessage message) async{
+//     print('Background Handler  ${message.messageId}');
 
-  }
+//   }
 
-   static Future _onMessageHandler(RemoteMessage message) async{
-    print('On Message Handler  ${message.messageId}');
+//    static Future _onMessageHandler(RemoteMessage message) async{
+//     print('On Message Handler  ${message.messageId}');
 
-  }
+//   }
 
-   static Future _onMessageOpenAPP(RemoteMessage message) async{
-    print('On Message Open App  ${message.messageId}');
+//    static Future _onMessageOpenAPP(RemoteMessage message) async{
+//     print('On Message Open App  ${message.messageId}');
 
-  }
+//   }
 
-  static Future initializeApp() async{
-    // push notifications
-    await Firebase.initializeApp();
-    token = await messaging.getToken();
-    print(token);
+//   static Future initializeApp() async{
+//     // push notifications
+//     await Firebase.initializeApp();
+//     token = await messaging.getToken();
+//     print(token);
 
-    // Handlers
-    FirebaseMessaging.onBackgroundMessage(_backgroundHandler);
-    FirebaseMessaging.onMessage.listen(_onMessageHandler);
-    FirebaseMessaging.onMessageOpenedApp.listen(_onMessageOpenAPP);
-  }
+//     // Handlers
+//     FirebaseMessaging.onBackgroundMessage(_backgroundHandler);
+//     FirebaseMessaging.onMessage.listen(_onMessageHandler);
+//     FirebaseMessaging.onMessageOpenedApp.listen(_onMessageOpenAPP);
+//   }
   
   
 
   
   
-}
+// }
 
 
 
